ae16804686dc7c8ebea01aae777a664dc4c3b379
Wellington Fonseca <64553168+W-Fonseca@users.noreply.github.com>
2024-02-09 10:46:56 -0300

    Update Return_All_Properties_in_SAP.vb

--- Return_All_Properties_in_SAP.vb
+++ Return_All_Properties_in_SAP.vb
@@ -5,6 +5,31 @@ Set App = SapGuiAuto.GetscriptingEngine
 Set Connection = App.Children(0)
 Set Session = Connection.Children(0)
 
+   Range("A1").Value = "SapObject.ContainerType"
+    Range("B1").Value = "SapObject.Id"
+    Range("C1").Value = "SapObject.Name"
+    Range("D1").Value = "SapObject.Parent"
+    Range("E1").Value = "SapObject.Type"
+    Range("F1").Value = "SapObject.TypeAsNumber"
+    Range("G1").Value = "SapObject.AccLabelCollection"
+    Range("H1").Value = "SapObject.AccText"
+    Range("I1").Value = "SapObject.AccTextOnRequest"
+    Range("J1").Value = "SapObject.AccTooltip"
+    Range("K1").Value = "SapObject.Changeable"
+    Range("L1").Value = "SapObject.DefaultTooltip"
+    Range("M1").Value = "SapObject.Height"
+    Range("N1").Value = "SapObject.IconName"
+    Range("O1").Value = "SapObject.IsSymbolFont"
+    Range("P1").Value = "SapObject.Left"
+    Range("Q1").Value = "SapObject.Modified"
+    Range("R1").Value = "SapObject.ParentFrame"
+    Range("S1").Value = "SapObject.ScreenLeft"
+    Range("T1").Value = "SapObject.ScreenTop"
+    Range("U1").Value = "SapObject.Text"
+    Range("V1").Value = "SapObject.Tooltip"
+    Range("W1").Value = "SapObject.Top"
+    Range("X1").Value = "SapObject.Width"
+
 i = 1
 
 For Each SapObject In Session.Children
